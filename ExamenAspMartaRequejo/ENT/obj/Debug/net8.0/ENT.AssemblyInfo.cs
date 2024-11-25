ddfc5363c6098eb82a5db3d632b8e5ee9f009c56
mreqmer <145675425+mreqmer@users.noreply.github.com>
2024-11-25 20:41:31 +0100

    Auto stash before merge of "main" and "origin/main"

--- ExamenAspMartaRequejo/ENT/obj/Debug/net8.0/ENT.AssemblyInfo.cs
+++ ExamenAspMartaRequejo/ENT/obj/Debug/net8.0/ENT.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ENT")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e3c3ce24ba4176603915166fd8821fb0494404a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92895cc0f3beb7effd57baf577260fca2ec0fd88")]
 [assembly: System.Reflection.AssemblyProductAttribute("ENT")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ENT")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
