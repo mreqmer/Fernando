180d1b627ae9baa4f876ca61cf62a50b182e52c3
mreqmer <145675425+mreqmer@users.noreply.github.com>
2024-12-02 07:18:14 +0100

    ---

--- ExamenAspMartaRequejo/DAL/obj/Debug/net8.0/DAL.AssemblyInfo.cs
+++ ExamenAspMartaRequejo/DAL/obj/Debug/net8.0/DAL.AssemblyInfo.cs
@@ -14,7 +14,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+<<<<<<< Updated upstream
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f1f6d900d0f08ff7e7999b44a034ef81394c9af")]
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92895cc0f3beb7effd57baf577260fca2ec0fd88")]
+>>>>>>> Stashed changes
 [assembly: System.Reflection.AssemblyProductAttribute("DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
